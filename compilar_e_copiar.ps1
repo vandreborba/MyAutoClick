e06e620eea62a5255d0b8cebb24e64e38e0ef381
Vandré <vandretrevisan@yahoo.com.br>
2025-06-26 10:33:35 -0300

    Corrigido arquivo compilar e copiar.

--- compilar_e_copiar.ps1
+++ compilar_e_copiar.ps1
@@ -22,21 +22,13 @@ if (Test-Path $configInterface) {
 }
 
 # Compila o projeto e copia o executável para a área de trabalho (PowerShell)
-<<<<<<< HEAD
-$pyinstaller = "pyinstaller"
-=======
-$pyinstaller = ".venv\\Scripts\\python.exe -m PyInstaller"
->>>>>>> master
+$pythonExe = ".venv\Scripts\python.exe"
 $nomeExe = "My IBGE AutoClicker.exe"
 $icone = "icon.ico"
 $main = "main.py"
 
 # Compilar
-<<<<<<< HEAD
-& $pyinstaller --onefile --name "My IBGE AutoClicker" --icon $icone $main
-=======
-Invoke-Expression "$pyinstaller --onefile --name 'My IBGE AutoClicker' --icon $icone $main"
->>>>>>> master
+& $pythonExe -m PyInstaller --onefile --name "My IBGE AutoClicker" --icon $icone $main
 
 # Caminhos
 $distPath = Join-Path $PSScriptRoot "dist"
